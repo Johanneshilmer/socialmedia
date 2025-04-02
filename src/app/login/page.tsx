fefe275008b126037cd87ad44497ecae8c219c22
johanneshilmer <johanneshilmerson94@gmail.com>
2025-04-02 21:24:17 +0200

    change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function page() {
-  return <div>Hello</div>;
+  return (
+    <div className="login-main">
+      <div className="login-form">
+        <div className="container login-header">
+          <h1>Login</h1>
+        </div>
+
+        <form
+          action="/api/login"
+          method="POST"
+          className="container login-inputs"
+        >
+          <input type="text" name="username" placeholder="Username" required />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+          <button type="submit">Login!</button>
+        </form>
+      </div>
+    </div>
+  );
 }
